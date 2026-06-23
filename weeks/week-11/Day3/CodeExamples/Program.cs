df37ee403a8a20dcecdb432ce0498ad6c75e1182
shlomoshoov <shloshoov@gmail.com>
2026-06-23 17:38:41 +0300

    day 3

---
+++ weeks/week-11/Day3/CodeExamples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Examples
+{
+    class Examples
+    {
+        enum Level
+        {
+            Low,
+            Medium
+        }
+       static void Main()
+
+        {
+            Level myVar = Level.Medium;
+            Console.WriteLine(myVar);
+            int n  = 0;
+            Try(out n);
+            Console.WriteLine(n);
+            int? nn = null;
+            Console.WriteLine(nn ?? 7);
+            //int* r = &n;
+            }
+        
+        static void Try(out int num)
+        {
+            num = 4;
+        }
+
+    }
+}
+
